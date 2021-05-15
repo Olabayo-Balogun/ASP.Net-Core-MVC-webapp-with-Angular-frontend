c9d916e8709413f441e2b7df394bcaf8b8088da8
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-05-15 19:47:49 +0100

    Successfully used entiry framework to create the database, a bespoke config file was created (and the "appsettings.json" fike deleted) in order to enable the process of building a config file from the ground up and explaining the process involved in making it happen. You specify the path to the SQL database (for the project) in the config file using key value pairs. In order to help the project recognise the location of the database and map to it, you have to create a constructor in your Context class (which for this project is the "DutchContext.cs" class),  pass in parameters by making use of the IConfiguration class and give it a name (which you create a private field for) after which you're able to use the name of the IConfiguration class you instantiated in the "optionBuilder" method (within DutchContext class) to map to the database. After all the above is done, you can begin your initial migration and update the database.
    All the necessary comments are available within the project to walk you throuh all the steps.

--- Data/DutchContext.cs
+++ Data/DutchContext.cs
@@ -1,5 +1,6 @@
 ﻿using DutchTreat.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,14 @@ namespace DutchTreat.Data
     //It's important to create a Context class and inherit from DbContext which will help you execute your queries to the database/data table 
     public class DutchContext : DbContext
     {
+        //The private field and the constructor is built to enable us get a connection string as a parameter for our SQL
+        private readonly IConfiguration _config;
+
+        public DutchContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
         //The property below is what is used to issue queries to the Products class/data table. It can also be used to add to the products in the database  
         //By convention, the name of these properties is the plural form of the class
         //We didn't create a DbSet for the "OrderItem" class because the "Order" class has a strong relationship with the "OrderItem" class and we don't need to query the "OrderItem" separately
@@ -22,7 +31,11 @@ namespace DutchTreat.Data
         {
             base.OnConfiguring(optionsBuilder);
 
-            optionsBuilder.UseSqlServer();
+            //Because you need the connection string as a parameter for the code below you have to create a constructor above and call in some other useful parameters
+            //The parameter passed in will involve looking for the "config.json" file and then the "ConnectionStrings" value-pair that we declared in that file
+            //If our key value-pair is deeply nested, we would have to use a lot of ":" to get to it in the same way we use the "/" to get to the path of a document
+            //The purpose of specifying the path is to pull the value of the "DutchContextDb" which was specified in the "config.json" file. The value of that key is a string that lets this method know where to find the database it'll be using
+            optionsBuilder.UseSqlServer(_config["ConnectionStrings:DutchContextDb"]);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,9 +19,30 @@ namespace DutchTreat
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                //The ".ConfigureAppConfiguration()" declaration simply tells the project that you want to configure application configuration (which usually happens in the "Startup.cs" class)
+                //Also not that I deleted the "appsettings.json" file in a bid to build it from the ground up and understand everything that is going on in that file
+                .ConfigureAppConfiguration(AddConfiguration)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        //Note that "ctx" means "context" (which is a parameter name we gave to "HostBuilderContext" class)
+        //Note that "bldr" means "builder" (which is a parameter name we gave to "IConfigurationBuilder" class)
+        //Also note that it isn't advisable to name your parameters this way, always favor clear words over abbreviations
+        private static void AddConfiguration(HostBuilderContext ctx, IConfigurationBuilder bldr)
+        {
+            //The line of code below gets rid of the need for the AppConfiguration file
+            //Note that you don't need to do these stuff in your normal projects, the knowledge however can be userful in future
+            bldr.Sources.Clear();
+
+            //The line of code below declares the location of a configuration file we intend to use in this project seeing as we deleted the scaffolded one in favor of learning how to build things from the ground up.
+            bldr.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json")
+                //Adding the ".AddEnvironmentVariables()" declaration is especially useful when you're dealing with cloud deployment to define configuration pieces
+                //This basically builds a configuration based on the sources you're putting into it,
+                //The order matters when chaining these methods, this "config.json" file can be a default that is overrideable by the ".AddEnvironmentVariables()" method especially when the method has it's own name or path
+                .AddEnvironmentVariables();
+        }
     }
 }
